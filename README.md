719763f95c3f923c626228c24dd3e0c9199a9a30
oigwe <oigwe@fliptrx.com>
2021-12-02 22:08:45 -0500

    add jest dev-dependency

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# todo-app-tdd
+# Todo App API
+
+## CRUD methods that are being used in a REST API
+- **C**reate HTTP POST
+- **R**ead   HTTP GET
+- **U**pdate HTTP PUT
+- **D**elete HTTP Delete
+
+## JEST
+- Jest is a javascript testing framework built by facebook
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 6000;
+const port = 7000;
 
 
 app.get("/", (req, res) => { // (req, res) === (request, response)
-    res.send("Hello World!");
+    res.json("Hello World!"); //res.send is changed to res.json in order to eventually return an api
 });
 
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
-})
+});
